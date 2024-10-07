796511a8e61bd84d13caee00f8addc692a0489bc
Kshitiz Kaintura <kshitizkaintura2004@gmail.com>
2024-10-07 08:31:08 +0530

    context-API basic

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,14 @@
+import Profile from "./Components/Profile"
+import Login from "./Components/Login"
+import UserContextProvider from "./Context/UserContextProvider"
+
+function App() {
+  return (
+      <UserContextProvider>
+        <Login />
+        <Profile />
+      </UserContextProvider>
+  )
+}
+
+export default App
